5d67da3598ddf82f0231fcfaea64344e216f05a6
Vinicius Cardoso <vini90fs@gmail.com>
2026-03-17 19:45:52 -0300

    branch triangulo pratica 1 pt 1

---
+++ pratica1completa/src/pratica1completa/maintriangulo.java
@@ -0,0 +1,28 @@
+package pratica1completa;
+import java.util.Scanner;
+
+public class maintriangulo {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+        double c1, c2;
+
+        System.out.print("Digite o valor do primeiro cateto: ");
+        c1 = entrada.nextDouble();
+
+        System.out.print("Digite o valor do segundo cateto: ");
+        c2 = entrada.nextDouble();
+
+        TrianguloRetangulo t = new TrianguloRetangulo();
+
+        t.setC1(c1);
+        t.setC2(c2);
+
+        System.out.println("Hipotenusa: " + t.calcularHipotenusa());
+        System.out.println("Area: " + t.calcularArea());
+
+
+	}
+
+}
